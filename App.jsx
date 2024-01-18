d5de3758f9c36d32a4802fc8999bde9ca90b8d3c
RohitPatra-2002 <rohitpatra2107@gmail.com>
2024-01-18 21:21:23 +0530

    commit done

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import Routes from './src/navigations/Routes';
+
+const App = () =>{
+    return (
+      <View style={{flex:1}}>
+        <Routes />
+      </View>
+    )
+  }
+
+export default App;
